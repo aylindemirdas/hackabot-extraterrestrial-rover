64c6d5694be071ebd5cbc5037fe2bbd62fff1eca
Jean Paul Fernandez <jpxoi@icloud.com>
2024-03-17 10:02:19 +0000

    Refactor Bluetooth serial port configuration and add camera rotation functionality

--- RemoteRoverControl/main.py
+++ RemoteRoverControl/main.py
@@ -3,7 +3,9 @@ import time
 import keyboard
 
 # Set the Bluetooth serial port
-bluetooth_port = '/dev/tty.ROVER_19'  # Change this to match your Bluetooth serial port
+# Change this to match your Bluetooth serial port
+bluetooth_port = '/dev/tty.ROVER_19'
+
 
 # Establish serial communication with the Bluetooth module
 try:
@@ -14,14 +16,21 @@ except serial.SerialException:
     exit()
 
 # Function to send commands to the rover
+
+
 def send_command(command):
     bluetooth.write(command.encode())
 
 # Function to control the rover at full power
+
+
 def full_power():
     send_command('F')
+    print("Full power activated.")
 
 # Function to control the rover at normal power
+
+
 def normal_power(command):
     send_command(command)
     print(f"Command sent: {command}")
@@ -29,11 +38,22 @@ def normal_power(command):
     send_command('S')
 
 # Function to stop the rover
+
+
 def stop():
     send_command('S')
+    print("Rover stopped.")
+
+
+def rotate_camera(position):
+    send_command(position)
+    print(f"Camera Position: {position}")
 
 # Function to control the rover using the keyboard
+
+
 def control_rover():
+    index = 0
     while True:
         command = None
         arrow_keys_mapping = {
@@ -43,6 +63,7 @@ def control_rover():
             "right": "R",
         }
 
+        positions = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
         key_event = keyboard.read_event()
         if key_event.event_type == "down":
             if key_event.name == 'esc':  # Press 'esc' to stop
@@ -50,8 +71,16 @@ def control_rover():
             elif key_event.name in arrow_keys_mapping:  # Arrow keys mapping
                 command = arrow_keys_mapping[key_event.name]
                 normal_power(command)
+            elif key_event.name == "command":
+                if index < len(positions) - 1:
+                    index = index + 1
+                else:
+                    index = 0
+                position = positions[index]
+                rotate_camera(position)
             elif key_event.name == 'space':  # Press 'space' for full power
                 full_power()
+
 
 # Main function
 if __name__ == "__main__":
